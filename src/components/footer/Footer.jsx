1c79843003d87e0463699b304728663361a8acb8
Edsoncortes40 <edcortes@macbook-pro.myfiosgateway.com>
2023-03-03 17:15:26 -0500

    fixing linkedin Link

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => {
       <div className='footer_socials'>
         <a href='https://instagram.com/edcortes.riv' target='_blank'><AiOutlineInstagram/></a>
         <a href='https://github.com/Edsoncortes40' target='_blank'><AiOutlineGithub/></a>
-        <a href='https://linkedin.com/Edsoncortes40' target="_blank"><AiOutlineLinkedin/></a>
+        <a href='https://www.linkedin.com/in/edsoncortes40/' target="_blank"><AiOutlineLinkedin/></a>
       </div>
 
       <div className='footer_copyright'>
