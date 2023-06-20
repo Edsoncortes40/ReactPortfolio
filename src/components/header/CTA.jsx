3e4255af39cd7633d534b5baaab343573c6aedc8
Edsoncortes40 <saul_cortes00@yahoo.com>
2023-06-20 16:44:11 -0400

    housecleaning

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,6 @@ import CV from 'assets/resume.pdf'
 const CTA = () => {
   return (
     <div className='CTA'>
-        <a href={CV} download="EdsonResume.pdf" className='btn'>Download CV</a>
         <a href="#contacts" className='btn btn-primary'>Contact Me</a>
     </div>
   )
